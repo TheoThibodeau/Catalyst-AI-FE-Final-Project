fe02da6aa8eb4fa949bdc391f66be0a2d386fc56
annamaynard <anna.maynard@gmail.com>
2023-07-14 15:29:04 -0400

    POETRY -> themes,categories,sentiment,emotion, GENERATE CO-authored-by: ANNA <anna.maynard@gmail.com> CO-authoredby: TEDDY <teddythibodeau@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Poetry from "./components/poetry"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-       
-      </div>
-      <h1>Catalyst</h1>
-      <div className="card">
-        
-      </div>
-     
+        <Poetry/>  
     </>
   )
 }
