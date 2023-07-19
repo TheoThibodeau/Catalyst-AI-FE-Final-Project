6169edb8520d11ff83eb8dbc6a8829cd09694e3c
annamaynard <anna.maynard@gmail.com>
2023-07-19 10:58:08 -0400

    FIXED BUGGS YAYAYAY, router and promptpost
     Co-Authored by : Theodore Thibodeau <teddythibodeau@gmail.com>
     Co-Authored by : Anna Maynard <anna.maynard@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import './App.css'
-import { Routes, Route, Router } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from './components/landing.jsx';
 import Movement from './components/mediums/movement.jsx';
 import Music from './components/mediums/music';
@@ -13,17 +13,17 @@ function App() {
     <>
     {/* <Landing /> */}
     {/* <Movement /> */}
-   <Router>
+   <BrowserRouter>
     <Routes>
         <Route path="/" element={<Landing />} />
         {/* <Route path ="/creativewriting" element={<CreativeWriting postId={postId}/>} />  */}
-        {/* <Route path ="/movement" element={<Movement postId={postId}/>} /> 
-        <Route path ="/music" element={<Music postId={postId}/>} /> 
-        <Route path ="/visualart" element={<VisualArt postId={postId}/>} />  */}
-        {/* <Route path ="/create" element={<Create postId={postId}/>} /> 
-        <Route path ="/archive" element={<Archive postId={postId}/>} /> */}
+        {/* <Route path ="/movement" element={<Movement postId={postId}/>} />  */}
+        {/* <Route path ="/music" element={<Music postId={postId}/>} />  */}
+        {/* <Route path ="/visualart" element={<VisualArt postId={postId}/>} />   */}
+        {/* <Route path ="/create" element={<Create postId={postId}/>} />  */}
+        {/* <Route path ="/archive" element={<Archive postId={postId}/>} /> */}
     </Routes>
-   </Router> 
+   </BrowserRouter> 
    
     </>
   )
