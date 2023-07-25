491ec031564af06b7842c8fb3b9f82ee609606e0
annamaynard <anna.maynard@gmail.com>
2023-07-25 13:13:51 -0400

    createpage in working condition

--- src/components/create.jsx
+++ src/components/create.jsx
@@ -27,69 +27,72 @@ const Create = ({ postId, output }) => {
   };
 
   return (
-    <div className="flex flex-col items-start w-full">
-      <div className=" p-4">
-        <div className="text-center border font-extralight text-xl pb-4">
+    <div className="flex flex-col items-center w-full p-4">
+      <div className="w-full lg:w-3/4 xl:w-1/2 pb-4">
+        <div className="text-center font-extralight text-xl">
           Prompt: {output}
         </div>
-        <div className="flex flex-row space-y-4">
+        <div className="flex flex-row space-y-4 mt-4">
           <button
-            className="w-full px-4 py-2 hover:underline text-gray-800 font-mono"
+            className="w-full px-4 py-2 hover:underline text-gray-800 font-mono text-center"
             onClick={() => setActiveComponent("Stopwatch")}
           >
             Stopwatch
           </button>
           <button
-            className="w-full px-4 py-2 hover:underline text-gray-800 font-mono "
+            className="w-full px-4 py-2 hover:underline text-gray-800 font-mono text-center"
             onClick={() => setActiveComponent("Audio")}
           >
             Audio Play
           </button>
           <button
-            className="w-full px-4 py-2 hover:underline text-gray-800 font-mono "
+            className="w-full px-4 py-2 hover:underline text-gray-800 font-mono text-center"
             onClick={() => setActiveComponent("Metronome")}
           >
             Metronome
           </button>
           <button
-            className="w-full px-4 py-2 hover:underline text-gray-800 font-mono "
+            className="w-full px-4 py-2 hover:underline text-gray-800 font-mono text-center"
             onClick={() => setActiveComponent("PomodoroTimer")}
           >
             Pomodoro Timer
           </button>
-
           <button
-            className="w-full px-4 py-2 hover:underline text-gray-800 font-mono "
+            className="w-full px-4 py-2 hover:underline text-gray-800 font-mono text-center"
             onClick={() => setActiveComponent("Dictionary")}
           >
             Dictionary
           </button>
         </div>
       </div>
-      <div className="w-full h-full flex flex-col items-center justify-center border p-4">{components[activeComponent]}</div>
-      <div className="justify-center">  
-         <Editor
-        apiKey='5c49z6msz9y26e8fv03ptpphkxydj6nybm6x070w4arrr81l'
-        onInit={(evt, editor) => editorRef.current = editor}
-        initialValue="<p>Take notes ... </p>"
-        init={{
-          height: 500,
-          menubar: false,
-          plugins: [
-            'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
-            'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
-            'insertdatetime', 'media', 'table', 'code', 'help', 'wordcount'
-          ],
-          toolbar: 'undo redo | blocks | ' +
-            'bold italic forecolor | alignleft aligncenter ' +
-            'alignright alignjustify | bullist numlist outdent indent | ' +
-            'removeformat | help',
-          content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
-        }}
-      />
-      <button className="p-4 border m-4"onClick={log}>Save</button></div>
+      <div className="w-full p-4 flex flex-grow lg:w-1/2 justify-center">
+        {components[activeComponent]}
+      </div>
+      <div className="w-full p-4">
+        <Editor
+          apiKey='5c49z6msz9y26e8fv03ptpphkxydj6nybm6x070w4arrr81l'
+          onInit={(evt, editor) => editorRef.current = editor}
+          initialValue="<p>Take notes ... </p>"
+          init={{
+            height: 500,
+            menubar: false,
+            plugins: [
+              'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
+              'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
+              'insertdatetime', 'media', 'table', 'code', 'help', 'wordcount'
+            ],
+            toolbar: 'undo redo | blocks | ' +
+              'bold italic forecolor | alignleft aligncenter ' +
+              'alignright alignjustify | bullist numlist outdent indent | ' +
+              'removeformat | help',
+            content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+          }}
+        />
+        <button className="p-4 border m-4 lg:w-auto block mx-auto" onClick={log}>
+          Save
+        </button>
+      </div>
     </div>
-
   );
 };
 
