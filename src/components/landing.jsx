2557c63a3bb035a68170ab3bc27dba2d22334682
annamaynard <anna.maynard@gmail.com>
2023-07-26 16:41:26 -0400

    landing,creativewriting,perametercomponent, CSS

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -4,34 +4,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 const Landing = () => {
-  const [output, setOutput] = useState('');
-  const [responseID, setResponseID] = useState('');
-
-    useEffect(()=> {
-        axios
-        .post('https://catalyst-x226.onrender.com/api/welcome/generate/')
-        .then((response) => {
-            setResponseID(response.data.id)
-        }) 
-        axios
-            .get(`https://catalyst-x226.onrender.com/api/welcome/${responseID}`)
-            .then((response) => {
-                setOutput(response.data.output)}) 
-    },[])
-  
-  
-    const BackgroundLines = () => (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="absolute top-20 left-0 w-full h-full"
-          preserveAspectRatio="none"
-          viewBox="0 0 100 100"
-        >
-          <line x1="0" y1="20" x2="100" y2="50" stroke="#ccc" strokeWidth="0.5" />
-          <line x1="0" y1="0" x2="20" y2="100" stroke="#ccc" strokeWidth="0.5" />
-        </svg>
-      );
-
+   
     const Navigate = useNavigate();
 
     const handleClickCreativeWriting = () => {
@@ -46,38 +19,49 @@ const Landing = () => {
       const handleClickVisualArt = () => {
         Navigate('/visualart');
       };
-    
+      
+      const handleClickCreate = () => {
+        Navigate('/create');
+      };
+     
+        const handleClickFolio = () => {
+            Navigate('/folio');
+        };
 
   return (
-    <>
-<BackgroundLines />
-     <div className="flex justify-end">
-     <h3 className="font-mono max-w-md p-10 mt-20 mr-5 pr-5 pb-5 text-right text-gray-500 bg-slate-200">
-        Welcome to our app! Discover a world of creativity and inspiration
-        through creative writing, music, movement, and visual art. Unleash your
-        imagination and embark on a journey of self-expression and growth with
-        our diverse range of prompts and activities. Let's start creating and
-        exploring together!"
-      </h3>
-      <div className="fixed bottom-0 text-left p-4">
-        <div>
-        <div className="flex flex-col gap-2 text-6xl text-slate-500 hover:text-slate-200 space-y-4 mb-10 ">
-            <Link to="/creativewriting"   className="hover:text-gray-500" onClick={handleClickCreativeWriting}>
-              CREATIVE WRITING
-            </Link>
-            <Link to="/movement" className="hover:text-gray-500" onClick={handleClickMovement}>
-              MOVEMENT
-            </Link>
-            <Link to="/music" className="hover:text-gray-500" onClick={handleClickMusic}>
-              MUSIC
-            </Link>
-            <Link to="/visualart" className="hover:text-gray-500" onClick={handleClickVisualArt}>
-              VISUAL ART
-            </Link>
-          </div>
+    
+<>
+      <div className="bg-slate-100 py-4 border border-slate-300">
+        <div className="pt-2 pb-4 space-x-4 text-right pr-4 text-slate-300 ">
+            <Link to="/create" onClick={handleClickCreate}>CREATE</Link>
+            <Link to="/create" onClick={handleClickFolio}>FOLIO</Link>
+        </div>
+        <div className="font-mono text-left text-6xl text-gray-500 pl-10 ">
+          <div className="">catalyst</div>
+          <div className="text-md pl-20 pt-3 ">artist, in the loop</div>
         </div>
       </div>
+
+      <div className="max-w-sm justify-center lg:max-w-2xl pl-10 px-6 py-5 lg:order-2">
+        <p>Welcome to our app! Discover a world of creativity and inspiration through creative writing, music, movement, and visual art. </p>
+        <br></br>
+        <p>Select a medium, choose your focus, narrow in, recive an AI generated prompt. Create. </p>
       </div>
+      <div className="flex flex-col gap-2 text-5xl text-slate-300 hover:text-slate-200 space-y-4 mb-10  pl-4 pb-20 pt-10 lg:pl-10">
+        <Link to="/creativewriting" className="hover:text-gray-400" onClick={handleClickCreativeWriting}>
+          CREATIVE WRITING
+        </Link>
+        <Link to="/movement" className="hover:text-gray-500" onClick={handleClickMovement}>
+          MOVEMENT
+        </Link>
+        <Link to="/music" className="hover:text-gray-500" onClick={handleClickMusic}>
+          MUSIC
+        </Link>
+        <Link to="/visualart" className="hover:text-gray-500" onClick={handleClickVisualArt}>
+          VISUAL ART
+        </Link>
+      </div>
+
     </>
   );
 };
