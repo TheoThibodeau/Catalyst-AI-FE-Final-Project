5e889cbf8b254d6d23c36e0955903866c3f21b04
annamaynard <anna.maynard@gmail.com>
2023-07-30 14:35:18 -0400

    folio

--- src/components/folio.jsx
+++ src/components/folio.jsx
@@ -1,23 +1,45 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useState } from 'react';
 import MusicFolio from './foliocomponents/musicfolio';
 import MovementFolio from './foliocomponents/movementfolio';
 import VisualArtFolio from './foliocomponents/visualartfolio';
 import CreativeWritingFolio from './foliocomponents/creativewritingfolio';
 
 const Folio = () => {
+  const [selectedComponent, setSelectedComponent] = useState(null);
+
+  const componentData = [
+    { name: 'creative', label: 'Writing' },
+    { name: 'movement', label: 'Movement' },
+    { name: 'music', label: 'Music' },
+    { name: 'visual', label: 'VisualArt' },
+  ];
+
+  const handleComponentClick = (component) => {
+    setSelectedComponent(component);
+  };
 
   return (
     <>
-      <div>
-
-      <h1>Folio</h1>   
-
+      
+      <div className="flex gap-4 pt-5 justify-center">
+        {componentData.map((component) => (
+          <button
+            key={component.name}
+            className={`${
+              selectedComponent === component.name ? 'border-blue-500' : 'border-gray-500'
+            } border p-2`}
+            onClick={() => handleComponentClick(component.name)}
+          >
+            {component.label}
+          </button>
+        ))}
       </div>
-        <MusicFolio />
-        <MovementFolio />
-        <VisualArtFolio />
-        <CreativeWritingFolio />
+      <div className="mt-4">
+        {selectedComponent === 'music' && <MusicFolio />}
+        {selectedComponent === 'movement' && <MovementFolio />}
+        {selectedComponent === 'visual' && <VisualArtFolio />}
+        {selectedComponent === 'creative' && <CreativeWritingFolio />}
+      </div>
     </>
   );
 };
--- src/components/foliocomponents/creativewritingfolio.jsx
+++ src/components/foliocomponents/creativewritingfolio.jsx
@@ -1,49 +1,76 @@
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import dayjs from 'dayjs';
 
+
 const CreativeWritingFolio = () => {
   const [folios, setFolios] = useState([]);
+  const [selectedFolio, setSelectedFolio] = useState(null);
+  const [selectedDate, setSelectedDate] = useState(null);
   const [output, setOutput] = useState([]);
-  const [created_at, setCreated_at] = useState([]);
   const [note, setNote] = useState([]);
+  const [category, setCategory] = useState([]); // Corrected variable name
+  const [somatic, setSomatic] = useState([]);
   const [theme, setTheme] = useState([]);
-  const [category, setCategory] = useState([]);
   const [emotion, setEmotion] = useState([]);
   const [sentiment, setSentiment] = useState([]);
   const [promptLength, setPromptLength] = useState([]);
-  const [creativeWritingVisible, setCreativeWritingVisible] = useState(false);
 
-  const handleCreativeWritingPost = (e) => {
-      axios
-        .get(`https://catalyst-x226.onrender.com/api/write/`)
-        .then((response) => {
-          setOutput(response.data[0].output);
-          setCreated_at(response.data[0].created_at);
-          setNote(response.data[0].note);
-          setTheme(response.data[0].theme);
-          setCategory(response.data[0].category);
-          setEmotion(response.data[0].emotion);
-          setSentiment(response.data[0].sentiment);
-          setPromptLength(response.data[0].promptLength);
-          setCreativeWritingVisible(true);
-        })
-        .catch((error) => console.error(error));
-    }
+  useEffect(() => {
+    axios
+      .get('https://catalyst-x226.onrender.com/api/write/')
+      .then((response) => {
+        setFolios(response.data);
+        setOutput(response.data[0].output);
+        setNote(response.data[0].note);
+        setSomatic(response.data[0].somatic);
+        setTheme(response.data[0].theme);
+        setEmotion(response.data[0].emotion);
+        setSentiment(response.data[0].sentiment);
+        setPromptLength(response.data[0].promptLength);
+        setCategory(response.data[0].category); // Corrected variable name
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  const handleDateClick = (date) => {
+    if (selectedDate === date) {
+        setSelectedDate(null);
+        setSelectedFolio(null);
+      } else {
+        setSelectedDate(date);
+        setSelectedFolio(folios.find((folio) => dayjs(folio.created_at).format('MM-DD-YYYY HH:mm:ss') === date));
+      }
+    };
 
   return (
     <>
       <div>
-
-      <button onClick={handleCreativeWritingPost}>Creative Writing</button>
+        {folios.map((folio) => (
+          <button
+            key={folio.id}
+            onClick={() =>
+              handleDateClick(dayjs(folio.created_at).format('MM-DD-YYYY HH:mm:ss'))
+            }
+          >
+            {dayjs(folio.created_at).format('MMMM D, YYYY - HH:mm')}
+          </button>
+        ))}
       </div>
 
-      {creativeWritingVisible && (
+      {selectedDate && (
         <div>
-          <h3>A.I Generate Prompt: {output}</h3>
-          <h3>Date of Creation: {dayjs(created_at).format('MM-DD-YYYY HH:mm:ss')}</h3>
-          <h3>Notes: {note}</h3>
-          <h3>Prompt Parameters: {theme} {category} {emotion} {sentiment} {promptLength}</h3>
+          <h3>Date of Creation: {selectedDate}</h3>
+          {selectedFolio && (
+            <>
+              <h3>A.I Generate Prompt:{output}</h3>
+              <h3>
+                Prompt Parameters: {theme} {category}{somatic}
+                {emotion} {sentiment} {promptLength}
+              </h3>
+              <h3>Notes: {note}</h3>
+            </>
+          )}
         </div>
       )}
     </>
