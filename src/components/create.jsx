380a88a755544d37282ccbff81183ef9311df935
annamaynard <anna.maynard@gmail.com>
2023-07-31 13:16:08 -0400

    edits to create, music dict, timer, pomodoro

--- src/components/create.jsx
+++ src/components/create.jsx
@@ -34,11 +34,11 @@ const Create = ({ postId, output }) => {
     <NavBar />
       <div className="flex flex-wrap items-center w-full ">
         <div className="w-full pb-4 pl-4 pr-4">
-          <div className="font-serif text-center p-7 pr-5 pl-5  bg-slate-100 border border-slate-500 font-extralight text-lg">
+          <div className="font-serif text-center p-8 pr-5 pl-5  bg-slate-100 border border-slate-500 font-extralight text-lg">
             {output}
           </div>
           <div>
-            <div className="grid grid-cols-2 gap-4 mt-2 pt-4">
+            <div className="grid grid-cols-2 gap-2 pt-4">
               <button
                 className="w-full px-4 py-2 border border-slate-400 text-gray-800 bg-slate-100 font-mono text-center"
                 onClick={() => setActiveComponent("Timer")}
@@ -67,7 +67,7 @@ const Create = ({ postId, output }) => {
           </div>
         </div>
       </div>
-      <div className="text- w-full p-4 pb-8 flex flex-grow lg:w-1/2 font-serif justify-center">
+      <div className="text- w-full pb-8 flex flex-grow lg:w-1/2 font-serif justify-center">
         {components[activeComponent]}
       </div>
     </>
